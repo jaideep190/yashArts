1ff07ca2a074d918d0918de7a64199d5c0c04e68
Lokesh Pallinti <pallinti.lokesh@gmail.com>
2025-06-30 19:38:45 +0000

    the main background of the website is plane static, can you make it more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya&family=Belleza&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased animated-gradient">
         <AuthProvider>
           <ImageKitProvider>
             {children}
